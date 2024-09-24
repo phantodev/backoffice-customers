180fa90a19c161afd4dd9c8e77c9af55ddf404b7
Eduardo Burko <kduburko@gmail.com>
2024-09-24 20:56:40 -0300

    finalzaido slides

--- src/app/dashboard/banners/page.tsx
+++ src/app/dashboard/banners/page.tsx
@@ -2,7 +2,12 @@
 
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Navigation } from 'swiper/modules'
 import 'swiper/css'
+import 'swiper/css/pagination'
+// import 'swiper/css/navigation'
+import '../../../styles/global.css'
+import { ArrowFatLeft, ArrowFatRight } from '@phosphor-icons/react'
 
 export default function BannersPage() {
   return (
@@ -14,6 +19,15 @@ export default function BannersPage() {
           onSlideChange={() => console.log('Mudei de slide')}
           onSwiper={(swiper) => console.log(swiper)}
           className="w-full"
+          modules={[Pagination, Navigation]}
+          pagination={{
+            clickable: true,
+            el: '.swiper-pagination',
+          }}
+          navigation={{
+            prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+          }}
         >
           {[1, 2, 3, 4].map((index) => (
             <SwiperSlide key={index}>
@@ -23,6 +37,13 @@ export default function BannersPage() {
             </SwiperSlide>
           ))}
         </Swiper>
+        <section className="swiper-pagination absolute bottom-0 w-full flex justify-center mt-6" />
+        <section className="swiper-button-prev absolute -left-6 top-1/2 transform -mt-4 -translate-y-1/2 z-10 cursor-pointer bg-emerald-600 p-4 rounded-full">
+          <ArrowFatLeft size={20} />
+        </section>
+        <section className="swiper-button-next absolute -right-6 top-1/2 transform -mt-4 -translate-y-1/2 z-10 cursor-pointer bg-emerald-600 p-4 rounded-full">
+          <ArrowFatRight size={20} />
+        </section>
       </section>
     </section>
   )
